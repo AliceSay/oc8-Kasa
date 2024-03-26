5e7948729283ae059c16d85b1c427dca31fe72f4
Alice <alicesay@hotmail.fr>
2024-03-26 11:29:06 +0100

    création des boutons left et right sur le caroussel

--- oc8-kasa/src/components/ApartmentBanner/ApartmentBanner.jsx
+++ oc8-kasa/src/components/ApartmentBanner/ApartmentBanner.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ApartmentBanner.css'
 
 function ApartmentBanner(props) {
   const pictures = props.pictures
+
+  const [currentPicture, setCurrentPicture] = useState(0)
+  const getClassName = (i) => {
+    if (i === currentPicture) return 'show'
+    return ''
+  }
+
+  const moveToNext = () => {
+    setCurrentPicture((currentPicture + 1) % pictures.length)
+  }
+
+  const moveToPrevious = () => {
+    const newCurrentPicture = currentPicture - 1
+    if (newCurrentPicture < 0) {
+      setCurrentPicture(pictures.length - 1)
+      return
+    }
+    setCurrentPicture(currentPicture - 1)
+  }
+
   return (
     <div className='apartment-img'>
-      {pictures.map((pic) => (
-        <img key={pic} src={pic} alt={pic} />
-      ))}
+      <div className='image__container'>
+        {pictures.map((pic, i) => (
+          <img key={pic} src={pic} alt='' className={getClassName(i)} />
+        ))}
+      </div>
+      <button className='btn btn-next' onClick={moveToNext}>
+        <i className='fas fa-chevron-left'></i>
+      </button>
+      <button className='btn btn-previous' onClick={moveToPrevious}>
+        <i className='fas fa-chevron-right'></i>
+      </button>
     </div>
   )
 }
